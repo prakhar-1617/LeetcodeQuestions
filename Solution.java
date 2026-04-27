b36a2611e17783e7a26fa95cae99745ea45f1333
prakhar kumar <prakharkumarcse@gmail.com>
2026-04-27 11:37:37 +0530

    added new codes

---
+++ Solution.java
@@ -0,0 +1,18 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+          node.val = node.next.val;
+        node.next = node.next.next;
+    }
+} {
+    
+}
